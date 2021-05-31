9fab1800f028bdb35e673d64aedfcdc41a0011f4
Billy Peake <peake100@gmail.com>
2021-05-30 20:38:26 -0700

    switched coverage to covertool

--- mix.exs
+++ mix.exs
@@ -8,14 +8,7 @@ defmodule Vtc.MixProject do
       description: "A SMPTE timecode library for Elixir",
       source_url: "https://github.com/opencinemac/vtc-ex",
       elixir: "~> 1.12",
-      test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [
-        coveralls: :test,
-        "coveralls.detail": :test,
-        "coveralls.post": :test,
-        "coveralls.html": :test,
-        "coveralls.xml": :test
-      ],
+      test_coverage: [tool: :covertool],
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       package: package(),
@@ -33,7 +26,7 @@ defmodule Vtc.MixProject do
     [
       {:ratio, "~> 2.0"},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
-      {:excoveralls, "~> 0.10", only: :test},
+      {:covertool, "~> 2.0"},
       {:junit_formatter, "~> 3.1", only: [:test]},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false}
     ]
