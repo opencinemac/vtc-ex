45f1ba8de68cae190a8f6e1f37b89ee06019239f
Billy Peake <peake100@gmail.com>
2023-06-16 01:57:04 -0700

    Update pg_module.ex with specs and comments.

--- lib/ecto/postgres/pg_module.ex
+++ lib/ecto/postgres/pg_module.ex
@@ -4,6 +4,7 @@ defmodule Vtc.Ecto.Postgres.Module do
   ## Exposes a macro for defining modules that will only be compiled if the caller
   ## has set `:vtc, :postgres_types?` to `true` in their application config.
 
+  @spec __using__(Keyword.t()) :: Macro.t()
   defmacro __using__(_) do
     quote do
       import Ecto.Postgres.Module
@@ -12,11 +13,14 @@ defmodule Vtc.Ecto.Postgres.Module do
     end
   end
 
+  # Wraps defmodule, conditionally declaring the module during compilation 
+  # based on caller configuration.
+  @spec defpgmodule(module(), do: Macro.t()) :: Macro.t()
   defmacro defpgmodule(name, do: body) do
     quote do
       if Application.get_env(:vtc, :postgres_types?, false) do
-        unquote(__MODULE__).check_dep(Ecto, :ecto)
-        unquote(__MODULE__).check_dep(Postgrex, :postgrex)
+        :ok = unquote(__MODULE__).enforce_dep(Ecto, :ecto)
+        :ok = unquote(__MODULE__).enforce_dep(Postgrex, :postgrex)
 
         defmodule unquote(name) do
           unquote(body)
@@ -25,9 +29,13 @@ defmodule Vtc.Ecto.Postgres.Module do
     end
   end
 
-  def check_dep(module, name) do
+  # Affirms that module from dep is present, throwing otherwise.
+  @spec enforce_dep(module(), atom()) :: :ok
+  def enforce_dep(module, name) do
     if not Code.ensure_loaded?(module) do
       throw("vtc: `:postgres_types` config is true, but `#{module}` module not found. Add `#{name}` to your dependencies")
     end
+
+    :ok
   end
 end
