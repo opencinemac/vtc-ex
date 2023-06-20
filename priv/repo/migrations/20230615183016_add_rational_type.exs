ac493007ec4d6b380738f263b9b2ff15717bb326
Billy Peake <peake100@gmail.com>
2023-06-20 12:57:26 -0700

    test running migration code twice

--- priv/repo/migrations/20230615183016_add_rational_type.exs
+++ priv/repo/migrations/20230615183016_add_rational_type.exs
@@ -8,5 +8,6 @@ defmodule Vtc.Test.Support.Repo.Migrations.AddRationalType do
 
   def change do
     :ok = PgRational.Migrations.create_all()
+    :ok = PgRational.Migrations.create_all()
   end
 end
