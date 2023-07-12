89c3be06b60be37c681c28129014d45f199044de
Billy Peake <peake100@gmail.com>
2023-07-12 11:58:47 -0700

    [Feature] Make Canonical Injection Optional (#71)

--- lib/ecto/postgres/postgres_migrations.ex
+++ lib/ecto/postgres/postgres_migrations.ex
@@ -18,13 +18,27 @@ defpgmodule Vtc.Ecto.Postgres.Migrations do
   - [PgFramerate](`Vtc.Ecto.Postgres.PgFramerate.Migrations`)
   - [PgFramestamp](`Vtc.Ecto.Postgres.PgFramestamp.Migrations`)
   - [PgFramestamp.Range](`Vtc.Ecto.Postgres.PgFramestamp.Range.Migrations`)
+
+  > #### Required Permissions {: .warning}
+  >
+  > To add the `framestamp_range`
+  > [canonical](https://www.postgresql.org/docs/current/rangetypes.html#RANGETYPES-DISCRETE),
+  > function, we must directly add it to the `framestamp_range` type in the `pg_catalog`
+  > table. In most databases, this will require elevated permissions. See the
+  > `PgFramestamp.Range.Migrations.inject_canonical_function/0` for more information on
+  > why this is required.
+  >
+  > You can choose to skip this step if you wish my setting the `inject_canonical?`
+  > op to false, but operations that require discreet nudging of in and out points will
+  > not return correct results, and ranges with different upper/lowwer bound types will
+  > not be comparable.
   """
-  @spec migrate() :: :ok
-  def migrate do
+  @spec migrate(Keyword.t()) :: :ok
+  def migrate(opts \\ []) do
     PgRational.Migrations.create_all()
     PgFramerate.Migrations.create_all()
     PgFramestamp.Migrations.create_all()
-    PgFramestamp.Range.Migrations.create_all()
+    PgFramestamp.Range.Migrations.create_all(opts)
 
     :ok
   end
